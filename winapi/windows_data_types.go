ca521262b4b1ee178ea86d54c295e21a5f5bffbd
SpikFriendNNter <123128838+SpikFriendNNter@users.noreply.github.com>
2023-04-03 14:00:35 -0300

    Update windows_data_types.go

--- winapi/windows_data_types.go
+++ winapi/windows_data_types.go
@@ -2,16 +2,16 @@ package winapi
 
 type HWND		uintptr
 type LONG		int32
-type LONGLONG	uint64
+type LONGLONG		uint64
 type WORD		uint16
 type DWORD		uint32
-type DWORDLONG	LONGLONG
+type DWORDLONG		LONGLONG
 
 
 const (
 	MONITOR_DEFAULTTOPRIMARY = 0x00000001
-	SRCCOPY 				 = int32(0x00CC0020)
-	BI_RGB					 = 0
+	SRCCOPY 			 = int32(0x00CC0020)
+	BI_RGB				 = 0
 	BI_BITFIELDS			 = 3
 	GMEM_MOVEABLE			 = 0x0002
 	DIB_RGB_COLORS 			 = 0x00
