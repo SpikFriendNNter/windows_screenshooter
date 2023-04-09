c8d0bbf5a145db3392772b02f9a8487482d51653
SpikFriendNNter <123128838+SpikFriendNNter@users.noreply.github.com>
2023-04-09 09:50:14 -0300

    Remove old code

--- main.go
+++ main.go
@@ -12,30 +12,14 @@ import (
 )
 
 func main() {
-	counter := 0
-	for {
-
-		img, err := print.Take_Screenshot()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var filename string = "out/image" + strconv.Itoa(counter) + ".png"
-		fmt.Println(filename)
-		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0777)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err = png.Encode(file, img); err != nil {
-			log.Fatal("SAVING IMAGE ERROR: ", err)
-		}
-
-		fmt.Println("IMAGE SAVED.")
-		file.Close()
-		counter++
-		time.Sleep(time.Second * 5)
+	img, err := print.Take_Screenshot()
+	file, err := os.OpenFile("screenshot.png", os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-
+	defer file.Close()
+	
+	if err = png.Encode(file, img); err != nil {
+		log.Fatal("ERROR SAVING IMAGE: ", err)
+	}
 }
