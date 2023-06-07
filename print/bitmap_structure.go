be56622bf70ea6e1a6a8d29308069cc470884a1d
SpikFriendNNter <123128838+SpikFriendNNter@users.noreply.github.com>
2023-06-06 21:25:33 -0300

    Update bitmap_structure.go

--- print/bitmap_structure.go
+++ print/bitmap_structure.go
@@ -1,28 +1,28 @@
 package print
 
 import (
-	"windows_screenshooter/winapi"
+	"github.com/SpikFriendNNter/windows_screenshooter/winapi"
 )
 
 type BITMAPINFOHEADER struct {
-  BiSize 		winapi.DWORD 
-  BiWidth 		winapi.LONG
-  BiHeight 		winapi.LONG  
-  BiPlanes 		winapi.WORD  
+  BiSize 			winapi.DWORD 
+  BiWidth 			winapi.LONG
+  BiHeight 			winapi.LONG  
+  BiPlanes 			winapi.WORD  
   BiBitCount 		winapi.WORD  
   BiCompression 	winapi.DWORD 
   BiSizeImage 		winapi.DWORD 
-  BiXPelsPerMeter     	winapi.LONG  
-  BiYPelsPerMeter     	winapi.LONG  
+  BiXPelsPerMeter	winapi.LONG  
+  BiYPelsPerMeter	winapi.LONG  
   BiClrUsed 		winapi.DWORD 
   BiClrImportant 	winapi.DWORD 
 }
 
 type RGBQUAD struct {
-  rgbBlue	 uint8
-  rgbGreen	 uint8
-  rgbRed	 uint8
-  rgbReserved    uint8
+  rgbBlue	 	uint8
+  rgbGreen	 	uint8
+  rgbRed	 	uint8
+  rgbReserved   uint8
 }
 
 type BITMAPINFO struct {
